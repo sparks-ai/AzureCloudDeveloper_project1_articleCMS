bc1b6562b5b9efac478aa63ab42741f1e96a9337
sparks-ai <58112524+sparks-ai@users.noreply.github.com>
2021-06-13 17:32:16 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'clouddevstorageacc'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or '70AyeR5woerAcfpT+S8PaiA5rhTjK4MaNdOV7owqbeZD/NuuCVBXKVaQU8KLdjzRy/9k2NZbWoKH4ca0TowdWQ=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'ENTER_SQL_SERVER_NAME.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'ENTER_SQL_DB_NAME'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ENTER_SQL_SERVER_USERNAME'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'clouddev-project-server.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'clouddev-project-db'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'clouddevadmin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'clouddevproject1!'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
